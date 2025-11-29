97afecb5a9a5e0d67028b6a7662944740fe45cc7
Julio Garcia <julio@gepgroup.es>
2025-11-29 19:39:12 +0100

    Add bulk update page for open training budgets (#1400)

---
+++ frontend/src/pages/recursos/OpenTrainingBulkUpdatePage.tsx
@@ -0,0 +1,7 @@
+import { OpenTrainingBulkUpdateView } from '../../features/recursos/OpenTrainingBulkUpdateView';
+
+export type OpenTrainingBulkUpdatePageProps = Record<string, never>;
+
+export default function OpenTrainingBulkUpdatePage(_props: OpenTrainingBulkUpdatePageProps) {
+  return <OpenTrainingBulkUpdateView />;
+}
