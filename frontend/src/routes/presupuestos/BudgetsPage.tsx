e147d19c5a0a10a4a74e1885eee9d64f0c6c9503
Julio Garcia <julio@gepgroup.es>
2025-12-04 01:09:33 +0100

    Revert "Show unplanned sessions missing trainers (#1523)" (#1526)
    This reverts commit 02d4eecbb527971d4a5d50de2c4b41bd1d4e5c6e.

--- frontend/src/routes/presupuestos/BudgetsPage.tsx
+++ frontend/src/routes/presupuestos/BudgetsPage.tsx
@@ -1,6 +1,5 @@
 import { BudgetSectionLayout, type BudgetSectionLayoutProps } from './BudgetSectionLayout';
 import { OpenTrainingUnplannedTable } from '../../features/presupuestos/OpenTrainingUnplannedTable';
-import { PendingTrainerSessionsTable } from '../../features/presupuestos/PendingTrainerSessionsTable';
 
 export type BudgetsPageProps = Omit<BudgetSectionLayoutProps, 'title' | 'subtitle'>;
 
@@ -12,15 +11,6 @@ export function BudgetsPage({ budgets, ...rest }: BudgetsPageProps) {
       title="Form. Empresa y GEP Services · Sin planificar"
       subtitle="Sube tu presupuesto y planifica"
       showFilters={false}
-      tableComponent={
-        <PendingTrainerSessionsTable
-          budgets={budgets}
-          isLoading={rest.isLoading}
-          isFetching={rest.isFetching}
-          error={rest.error}
-          onRetry={rest.onRetry}
-        />
-      }
     >
       <OpenTrainingUnplannedTable budgets={budgets} />
     </BudgetSectionLayout>
