27e49077efa8934df1e37b93e124a82a0e73473a
Julio Garcia <julio@gepgroup.es>
2026-01-29 18:31:56 +0100

    Update mailchimp person organization details (#2038)

--- frontend/src/types/mailchimpPerson.ts
+++ frontend/src/types/mailchimpPerson.ts
@@ -4,6 +4,7 @@ export type MailchimpPerson = {
   email: string | null;
   label_ids: string[];
   org_id: string | null;
+  org_name: string | null;
   org_address: string | null;
   size_employees: string | null;
   segment: string | null;
